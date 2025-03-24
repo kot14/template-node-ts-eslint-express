aa570d8f5eed5f1fc3ce6472bd90d43a53abac57
Yaroslav Mostovoy <mostovy.yaroslav.igorovich@gmail.com>
2025-03-24 20:24:03 +0200

    separating-routes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-import express, { Request, Response, Application } from "express";
+import express, { Application } from "express";
 import "dotenv/config";
+
+import { routes } from "./routes";
+
+
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+app.use(routes);
 
 
 app.listen(process.env.PORT || 3000, () => {
